8f6fb49d8162a2cff11603daed0489e6332a82b0
Rafael98-stack <rafaelcasapaojr@gmail.com>
2024-02-02 15:21:54 +0100

    Developing2.2

--- src/main/java/it/be/epicode/Entities/TrasportoPubblico/ALFABETO.java
+++ src/main/java/it/be/epicode/Entities/TrasportoPubblico/ALFABETO.java
@@ -3,3 +3,46 @@ package it.be.epicode.Entities.TrasportoPubblico;
 public enum ALFABETO {
     A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z
 }
+
+//package ILMIODAO;
+//
+//import it.be.epicode.Entities.TrasportoPubblico.Tratta;
+//
+//import javax.persistence.EntityManager;
+//import javax.persistence.EntityTransaction;
+//
+//public class TrattaDAO {
+//    private EntityManager em;
+//
+//    public TrattaDAO(EntityManager em){this.em = em;}
+//
+//    public void save(Tratta tratta) {
+//        try{
+//            EntityTransaction transaction = em.getTransaction();
+//            transaction.begin();
+//            em.merge(tratta);
+//            transaction.commit();
+//            System.out.println( tratta + "salvato correttamente");
+//        }catch (Exception e){
+//            System.out.println(e.getMessage());
+//        }
+//    }
+//
+////    public void save1(Auto_Bus auto_bus) {
+////        try{
+////            if (auto_bus.getNomeVeicolo() == auto_bus.getNomeVeicolo()) {
+////                System.out.println( auto_bus + "Esiste gia' nella raccolta");
+////            } else {
+////                EntityTransaction transaction = em.getTransaction();
+////                transaction.begin();
+////                em.merge(auto_bus);
+////                transaction.commit();
+////                System.out.println(auto_bus + "salvato correttamente");
+////            }
+////        }catch (Exception e){
+////            System.out.println(e.getMessage());
+////        }
+////    }
+
+
+
